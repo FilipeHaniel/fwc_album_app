b0e872d50d575956b47932f97bc172088df34bfc
FilipeHaniel <filipe.h.martins@timwe.com>
2023-10-12 00:56:22 -0300

    pattern styles

---
+++ lib/app/core/styles/colors_app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static ColorsApp? _instance;
+
+  ColorsApp._();
+
+  static ColorsApp get i {
+    _instance ??= ColorsApp._();
+
+    /// Força não ser nulo (!)
+    return _instance!;
+  }
+
+  Color get primary => const Color(0XFF791435);
+  Color get secondary => const Color(0XFFFDCE50);
+  Color get yellow => const Color(0XFFFDCE50);
+  Color get grey => const Color(0XFFFCCCCC);
+  Color get darkGrey => const Color(0XFF999999);
+}
+
+extension ColorsAppExtensions on BuildContext {
+  ColorsApp get colors => ColorsApp.i;
+}
--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/core/styles/colors_app.dart';
+import 'package:fwc_album_app/app/core/styles/text_styles.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          centerTitle: true,
-          iconTheme: IconThemeData(color: Colors.black),
-          titleTextStyle: TextStyle(color: Colors.black),
-        ),
-      ),
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Splash page')),
-        body: Container(),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Splash page')),
+      body: Container(),
     );
   }
 }
