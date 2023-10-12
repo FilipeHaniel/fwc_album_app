f1b0da4b0aa5a37d40b1f06f40762087f33f29ee
FilipeHaniel <filipe.h.martins@timwe.com>
2023-10-12 13:13:24 -0300

    createing components button

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fwc_album_app/app/core/styles/colors_app.dart';
-import 'package:fwc_album_app/app/core/styles/text_styles.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
