a0367254c19fe7a5fc8d96ffc6e6dddba5447df5
FilipeHaniel <filipe.h.martins@timwe.com>
2023-10-22 04:48:14 -0300

    implements form rgister validate and presenter

---
+++ lib/app/pages/auth/register/presenter/register_presenter_impl.dart
@@ -0,0 +1,43 @@
+import 'dart:developer';
+
+import 'package:fwc_album_app/app/models/register_user_model.dart';
+import 'package:fwc_album_app/app/pages/auth/register/presenter/register_presenter.dart';
+import 'package:fwc_album_app/app/pages/auth/register/view/register_view.dart';
+import 'package:fwc_album_app/app/repositories/auth/auth_repository.dart';
+
+class RegisterPresenterImpl implements RegisterPresenter {
+  final AuthRepository _authRepository;
+
+  late final RegisterView _view;
+
+  RegisterPresenterImpl({required AuthRepository authRepository})
+      : _authRepository = authRepository;
+
+  @override
+  Future<void> register({
+    required String name,
+    required String email,
+    required String password,
+    required String confirmPassword,
+  }) async {
+    final registerUserModel = RegisterUserModel(
+      name: name,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+    );
+
+    try {
+      await _authRepository.register(registerUserModel);
+
+      _view.registerSuccess();
+    } on Exception catch (e, s) {
+      log('Erro ao cadastrar usuário', error: e, stackTrace: s);
+
+      _view.registerSuccess();
+    }
+  }
+
+  @override
+  set view(RegisterView view) => _view = view;
+}
--- lib/app/pages/auth/register/register_page.dart
+++ lib/app/pages/auth/register/register_page.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
+import 'package:fwc_album_app/app/pages/auth/register/presenter/register_presenter.dart';
+import 'package:validatorless/validatorless.dart';
 
 class RegisterPage extends StatefulWidget {
-  const RegisterPage({super.key});
+  final RegisterPresenter presenter;
+
+  const RegisterPage({
+    required this.presenter,
+    super.key,
+  });
 
   @override
   State<RegisterPage> createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _name = TextEditingController();
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+  final _confirmPassword = TextEditingController();
+
+  @override
+  void dispose() {
+    _name.dispose();
+    _email.dispose();
+    _password.dispose();
+    _confirmPassword.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,33 +64,66 @@ class _RegisterPageState extends State<RegisterPage> {
                 child: Column(
                   children: [
                     TextFormField(
+                      controller: _name,
                       decoration: const InputDecoration(
                         label: Text('Nome completo *'),
                       ),
+                      validator: Validatorless.required('Obrigatório'),
                     ),
                     const SizedBox(height: 20),
                     TextFormField(
+                      controller: _email,
                       decoration: const InputDecoration(
                         label: Text('E-mail *'),
                       ),
+                      validator: Validatorless.multiple([
+                        Validatorless.required('Obrigatório'),
+                        Validatorless.email('E-mail inválido'),
+                      ]),
                     ),
                     const SizedBox(height: 20),
                     TextFormField(
+                      controller: _password,
                       decoration: const InputDecoration(
                         label: Text('Senha *'),
                       ),
+                      validator: Validatorless.multiple([
+                        Validatorless.required('Obrigatório'),
+                        Validatorless.min(
+                            6, 'Campo deve conter pelo menos 6 caractéres'),
+                      ]),
                     ),
                     const SizedBox(height: 20),
                     TextFormField(
+                      controller: _confirmPassword,
                       decoration: const InputDecoration(
                         label: Text('Confirma senha *'),
                       ),
+                      validator: Validatorless.multiple([
+                        Validatorless.required('Obrigatório'),
+                        Validatorless.min(
+                            6, 'Campo deve conter pelo menos 6 caractéres'),
+                        Validatorless.compare(
+                            _password, 'confirma senha diferente de senha'),
+                      ]),
                     ),
                     const SizedBox(height: 20),
                     Button.primary(
                       label: 'Cadastrar',
                       width: MediaQuery.of(context).size.width * .9,
-                      onPressed: () {},
+                      onPressed: () {
+                        final isFormValid =
+                            _formKey.currentState?.validate() ?? false;
+
+                        if (isFormValid) {
+                          widget.presenter.register(
+                            name: _name.text,
+                            email: _email.text,
+                            password: _password.text,
+                            confirmPassword: _confirmPassword.text,
+                          );
+                        }
+                      },
                     ),
                   ],
                 ),
